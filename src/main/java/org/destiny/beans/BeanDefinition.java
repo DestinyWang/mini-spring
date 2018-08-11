b98d693b8fec25d8e51953d26fcbd1ba29de9656
destiny <destinywk@163.com>
2018-08-11 16:13:06 +0800

    replace username

--- src/main/java/org/destiny/beans/BeanDefinition.java
+++ src/main/java/org/destiny/beans/BeanDefinition.java
@@ -2,12 +2,11 @@ package org.destiny.beans;
 
 /**
  * @author 王康
- * hzwangkang1@corp.netease.com
+ * destinywk@163.com
  * ------------------------------------------------------------------
  * <p></p>
  * ------------------------------------------------------------------
- * Corpright 2018 Netease, Inc. All rights reserved.
- * NETEASE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
  * @version JDK 1.8.0_101
  * @since 2018/8/6 22:45
  */
--- src/test/java/org/destiny/test/v1/BeanFactoryTest.java
+++ src/test/java/org/destiny/test/v1/BeanFactoryTest.java
@@ -14,12 +14,11 @@ import static org.junit.Assert.assertNotNull;
 
 /**
  * @author 王康
- * hzwangkang1@corp.netease.com
+ * destinywk@163.com
  * ------------------------------------------------------------------
  * <p></p>
  * ------------------------------------------------------------------
- * Corpright 2018 Netease, Inc. All rights reserved.
- * NETEASE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
  * @version JDK 1.8.0_101
  * @since 2018/8/6 22:33
  */
